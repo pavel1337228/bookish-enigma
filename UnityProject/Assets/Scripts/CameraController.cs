c9761169882c10475ca19d51a7db8a163043aeae
pavel1337228 <64475489+pavel1337228@users.noreply.github.com>
2021-03-20 17:01:45 +0300

    Summary

---
+++ UnityProject/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject eye;
+    public float transformx;
+    public float transformz;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Camera.main.transform.position = new Vector3(eye.transform.position.x, 10, eye.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        //Mathf.Lerp
+        float x; float z;
+        x = Mathf.Lerp(Camera.main.transform.position.x, eye.transform.position.x + transformx, 2f * Time.deltaTime);
+        z = Mathf.Lerp(Camera.main.transform.position.z, eye.transform.position.z + transformz, 2f * Time.deltaTime);
+        Camera.main.transform.position = new Vector3(x, 10, z);
+    }
+}

5db7cc6173de73ce484cb2e87833458de5604e21
pavel1337228 <64475489+pavel1337228@users.noreply.github.com>
2021-03-21 15:50:21 +0300

    Update

--- UnityProject/Assets/Scripts/CharacterController.cs
+++ UnityProject/Assets/Scripts/CharacterController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     [SerializeField] private Transform _pointTransform;
+    [SerializeField] private Rigidbody _playerTransform;
     private float _horizontal, _vertical, _moveAmount;
     private Animator _anim;
 
@@ -24,10 +25,38 @@ public class CharacterController : MonoBehaviour
         PlayAnimation();
     }
 
+    [SerializeField] private float _multiplayerz = 0f;
+    [SerializeField] private float _multiplayerx = 0f;
+
+    private void FixedUpdate()
+    {
+        _vertical = Input.GetAxis("Vertical");
+        _horizontal = Input.GetAxis("Horizontal");
+
+        _multiplayerz = Mathf.Lerp(0.2f, Mathf.Abs(_vertical), 50f * Time.deltaTime);
+        _multiplayerx = Mathf.Lerp(0.2f, Mathf.Abs(_horizontal), 50f * Time.deltaTime);
+        //_playerTransform.drag = Mathf.Lerp(3f, 2.5f * Mathf.Abs(_vertical), 20f *_vertical * Time.deltaTime);
+
+
+        if ((Mathf.Abs(_vertical) > 0f) & (Mathf.Abs(_horizontal) > 0f))
+        {
+            _playerTransform.AddForce(Vector3.forward * _vertical * Time.deltaTime * (_multiplayerz / 1.35f));
+            _playerTransform.AddForce(Vector3.right * _horizontal * Time.deltaTime * (_multiplayerx / 1.35f));
+        }
+        else
+        {
+            _playerTransform.AddForce(Vector3.forward * _vertical * Time.deltaTime * _multiplayerz);
+            _playerTransform.AddForce(Vector3.right * _horizontal * Time.deltaTime * _multiplayerx);
+        }
+
+    }
+
     public void Moving()
     {
         _vertical = Input.GetAxis("Vertical");
         _horizontal = Input.GetAxis("Horizontal");
+
+    
 
         Vector3 moveDir = _pointTransform.forward * _vertical;
         moveDir += _pointTransform.right * _horizontal;
